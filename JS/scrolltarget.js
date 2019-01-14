d084f2207e42868ba920846ba2a3fcc95a9ff67e
jon <43426006+jcwalp@users.noreply.github.com>
2019-01-14 14:31:29 -0500

    v.16

--- JS/scrolltarget.js
+++ JS/scrolltarget.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
   $('#icon-2').css('opacity', 0);
   $('#left-list').css('opacity', 0);
-
+  $('#right-list').css('opacity', 0);
+  $('#right-text').css('opacity', 0);
   $('#icon-2').waypoint(function() {
-      $('#icon-2').addClass('fadeInLeftBig icon-2');
+      $('#icon-2').addClass('fadeInLeft icon-2');
       $('#icon-2').css('opacity', 1);
   }, { offset: '30%' });
   $('#left-list').waypoint(function() {
-      $('#left-list').addClass('fadeInLeftBig left-list');
+      $('#left-list').addClass('fadeInLeft left-list');
       $('#left-list').css('opacity', 1);
   }, { offset: '100%' });
-
+  $('#right-list').waypoint(function() {
+      $('#right-list').addClass('fadeInLeft right-list');
+      $('#right-list').css('opacity', 1);
+  }, { offset: '100%' });
+  $('#right-text').waypoint(function() {
+      $('#right-text').addClass('fadeInRight right-text');
+      $('#right-text').css('opacity', 1);
+  }, { offset: '100%' });
 });
