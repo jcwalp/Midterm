58c8c29b3ab74c89a6beb6e3e72a80fbc6d1c597
jon <43426006+jcwalp@users.noreply.github.com>
2019-01-14 12:52:02 -0500

    v.14

--- JS/scrolltarget.js
+++ JS/scrolltarget.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+  $('#left-image').css('opacity', 0);
 
   $('#icon-2').waypoint(function() {
-      $('#icon-2').addClass('bounceInDown icon-2');
+      $('#icon-2').addClass('fadeInLeftBig icon-2');
+      $('#left-image').css('opacity', 1);
   }, { offset: '30%' });
 
 });
