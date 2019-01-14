6fa08e00c45b6186f893fd739e5b6aa059e87554
jon <43426006+jcwalp@users.noreply.github.com>
2019-01-14 16:59:15 -0500

    v.17

--- JS/scrolltarget.js
+++ JS/scrolltarget.js
@@ -1,22 +1,17 @@
 $(document).ready(function(){
   $('#icon-2').css('opacity', 0);
-  $('#left-list').css('opacity', 0);
-  $('#right-list').css('opacity', 0);
-  $('#right-text').css('opacity', 0);
+  $('#col-1').css('opacity', 0);
+  $('#image-sub').css('opacity', 0);
   $('#icon-2').waypoint(function() {
       $('#icon-2').addClass('fadeInLeft icon-2');
       $('#icon-2').css('opacity', 1);
   }, { offset: '30%' });
-  $('#left-list').waypoint(function() {
-      $('#left-list').addClass('fadeInLeft left-list');
-      $('#left-list').css('opacity', 1);
+  $('#col-1').waypoint(function() {
+      $('#col-1').addClass('bounceInLeft col-1');
+      $('#col-1').css('opacity', 1);
   }, { offset: '100%' });
-  $('#right-list').waypoint(function() {
-      $('#right-list').addClass('fadeInLeft right-list');
-      $('#right-list').css('opacity', 1);
-  }, { offset: '100%' });
-  $('#right-text').waypoint(function() {
-      $('#right-text').addClass('fadeInRight right-text');
-      $('#right-text').css('opacity', 1);
+  $('#image-sub').waypoint(function() {
+      $('#image-sub').addClass('fadeInUp');
+      $('#image-sub').css('opacity', 1);
   }, { offset: '100%' });
 });
