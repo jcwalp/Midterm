dab67a01c43468dcced2dabeb4b1821c5aa6d4f7
jon <43426006+jcwalp@users.noreply.github.com>
2019-01-14 13:07:42 -0500

    v.15

--- JS/scrolltarget.js
+++ JS/scrolltarget.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-  $('#left-image').css('opacity', 0);
+  $('#icon-2').css('opacity', 0);
+  $('#left-list').css('opacity', 0);
 
   $('#icon-2').waypoint(function() {
       $('#icon-2').addClass('fadeInLeftBig icon-2');
-      $('#left-image').css('opacity', 1);
+      $('#icon-2').css('opacity', 1);
   }, { offset: '30%' });
+  $('#left-list').waypoint(function() {
+      $('#left-list').addClass('fadeInLeftBig left-list');
+      $('#left-list').css('opacity', 1);
+  }, { offset: '100%' });
 
 });
